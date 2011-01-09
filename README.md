4c1deec9f973ca94d5a9b57a4bf03223b150234f
Thomas Rampelberg <thomas@bittorrent.com>
2011-01-08 16:38:31 -0800

    Initial commit, yes I know there aren't any unit tests yet. I'll get to it.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Getting started
+
+First, you'll need to include some new script tags on your pages:
+
+    <script src="underscore.js"></script>
+    <script src="jquery.js"></script>
+    <script src="jquery.couch.js"></script>
+    <script src="backbone.js"></script>
+    <script src="backbone.couchdb.js"></script>
+
+Then, in your app, add a little bit of config:
+
+    Backbone.couch.options.database = 'my_database';
+    Backbone.couch.options.design = 'my_design_document';
+
+## Models
+
+There's nothing that you'll need to do to make this work with models. The model
+id is going to be used to fetch/save the model and it will just work.
+
+## Collections
+
+For collections, there's some extra configuration required:
+
+    Backbone.Collection.extend({
+        couch: function() {
+            return {
+                view: 'my_view'
+            }
+        }
+    })
+
+The couch function needs to return options to query the db with. In this
+example, The results of
+`http://localhost:5984/my_database/_design/my_design_document/_view/my_view`
+will be added to the collection (with each row being a separate model).
+
+For a lot of views, it makes sense to have them look something like this:
+
+    emit(key, null);
+
+This ends up saving disk space if you're just using the raw document (as it can
+be fetched with the include_docs query parameter). If you'd like to do this
+trick here:
+
+    { view: 'my_view',
+      include_docs: 'true'
+    }
+
+Note that the couch function can return just about any query parameter. I find
+it especially useful for limiting the collection size and using startKey/endKey.
+
---
+++ backbone.couchdb.js
@@ -0,0 +1,62 @@
+/**
+ * @author: Thomas Rampelberg <thomas@saunter.org>
+ *
+ * Copyright(c) 2011 Thomas Rampelberg
+ */
+
+(function() {
+
+  Backbone.couch = {};
+  Backbone.couch.options = {
+    database: 'test',
+    design: 'backbone'
+  };
+
+  var couch = {
+    _db: null,
+    model: {
+      create: function(model, success, error) {
+        couch._db.saveDoc(model.toJSON(), {
+          success: success,
+          error: error
+        });
+      },
+      update: function(model, success, error) {
+        this.create(model, success, error);
+      },
+      read: function(model, success, error) {
+        couch._db.openDoc(model.id, {
+          success: success,
+          error: error
+        });
+      },
+      'delete': function(model, success, error) {
+      }
+    },
+    collection: {
+      read: function(model, success, error) {
+        var opts = model.couch();
+        var query = opts.design || Backbone.couch.options.design + '/' +
+          opts.view;
+        couch._db.view(query, _.extend({
+          success: function(resp) {
+            success(_.map(resp.rows, function(row, k) {
+              _.extend(row, row.doc);
+              if (!row.id && row._id) row.id = row._id;
+              delete row._id;
+              if (!row.id) row.id = k;
+              return row;
+            }));
+          },
+          error: error
+        }, opts));
+      }
+    }
+  };
+
+  Backbone.sync = function(method, model, success, failure) {
+    if (!couch._db) couch._db = $.couch.db(Backbone.couch.options.database);
+    var type = 'model' in model ? 'collection' : 'model';
+    couch[type][method](model, success, failure);
+  };
+})();
