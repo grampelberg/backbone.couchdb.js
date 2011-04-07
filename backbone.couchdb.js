1cdbaca1348cdfd570a149a3f7c1a5292c32ae2d
Thomas Rampelberg <thomas@bittorrent.com>
2011-04-06 19:02:15 -0700

    A bunch of changes to get this working with the newest backbone and allow multiple dbs in one app

--- backbone.couchdb.js
+++ backbone.couchdb.js
@@ -13,57 +13,66 @@
   };
 
   var couch = {
-    _db: null,
     model: {
-      create: function(model, success, error) {
-        couch._db.saveDoc(model.toJSON(), {
-          success: success,
-          error: error
+      create: function(_db, model, cb) {
+        _db.saveDoc(model.toJSON(), {
+          success: cb.success,
+          error: cb.error
         });
       },
-      update: function(model, success, error) {
-        this.create(model, success, error);
+      update: function(_db, model, cb) {
+        this.create(_db, model, cb);
       },
-      read: function(model, success, error) {
-        couch._db.openDoc(model.id, {
-          success: success,
-          error: error
+      read: function(_db, model, cb) {
+        _db.openDoc(model.id, {
+          success: cb.success,
+          error: cb.error
         });
       },
-      'delete': function(model, success, error) {
-        couch._db.removeDoc(model.toJSON(), {
-          success: success,
-          error: error
+      'delete': function(_db, model, cb) {
+        _db.removeDoc(model.toJSON(), {
+          success: cb.success,
+          error: cb.error
         });
       }
     },
     collection: {
-      read: function(model, success, error) {
+      read: function(_db, model, cb) {
         var opts = model.couch();
         if (!('view' in opts))
           throw new Error("The return of `couch()` must contain the view");
         var query = opts.design || Backbone.couch.options.design + '/' +
           opts.view;
         delete opts.view;
-        couch._db.view(query, _.extend({
+        _db.view(query, _.extend({
           success: function(resp) {
-            success(_.map(resp.rows, function(row, k) {
-              _.extend(row, row.doc);
+            cb.success(_.map(resp.rows, function(row, k) {
+              _.extend(row, row.value, row.doc);
               if (!row.id && row._id) row.id = row._id;
               delete row._id;
               if (!row.id) row.id = k;
               return row;
             }));
           },
-          error: error
+          error: cb.error
         }, opts));
       }
+    },
+    sync: function(method, model, cb) {
+      // XXX - This is going to be a memory issue unless someone does the
+      // extend trick.
+      if (!this._db) this._db = $.couch.db(Backbone.couch.options.database);
+      var type = 'model' in model ? 'collection' : 'model';
+      couch[type][method](this._db, model, cb);
     }
   };
 
-  Backbone.sync = function(method, model, success, failure) {
-    if (!couch._db) couch._db = $.couch.db(Backbone.couch.options.database);
-    var type = 'model' in model ? 'collection' : 'model';
-    couch[type][method](model, success, failure);
-  };
+  Backbone.couch.Model = Backbone.Model.extend({
+    sync: couch.sync
+  });
+
+  Backbone.couch.Collection = Backbone.Collection.extend({
+    sync: couch.sync
+  });
+
 })(jQuery);
