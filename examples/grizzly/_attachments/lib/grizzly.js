a60826775e912ab3326b9ed2ee34d25ea90b6361
Thomas Rampelberg <thomas@saunter.org>
2011-04-10 10:31:16 -0700

    Working on getting a soca example setup.

---
+++ examples/grizzly/_attachments/lib/grizzly.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2011, Thomas Rampelberg <thomas@saunter.org>
+ */
+
+(function($) {
+
+  var grizzy = {
+  };
+
+  window.grizzy = grizzly;
+
+})(jQuery);
---
+++ examples/grizzly/hooks/before_build.rb
@@ -0,0 +1,4 @@
+# any pre build scripting or plugins should go here
+plugin 'coffeescript'
+plugin 'jim'
+plugin 'compass'
